05cf5020cfabb83c36f24a3ab11fb6b3f6fadc5e
Felipe0Mota <105669915+Felipe0Mota@users.noreply.github.com>
2024-07-11 18:26:05 -0300

    adicionar repetição gerar numero aleatório // adicionar alteração no texto "Números sorteados"

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
+    
     let sorteados = [];
+    let numero;
 
-    for (let i = 1; i <= quantidade; i++) {
-        
+    for (let i = 0; i <= quantidade; i++) {
+        numero = obterNumeroAleatorio(de,ate);
+        sorteados.push(numero);
     }
 
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = ˋ<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>ˋ;
 }
 
 function obterNumeroAleatorio(min, max) {
