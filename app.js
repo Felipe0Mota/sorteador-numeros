2831711a9cf432e14250905651b655eb70418009
Felipe Wolff Mota <felipemotaw03@gmail.com>
2024-07-12 12:25:15 -0300

    adicionar função botão Reiniciar // adicionar condicionais para não ocorrer erros

--- app.js
+++ app.js
@@ -1,22 +1,96 @@
-let lista = []
+let quantidade;
+let de;
+let ate;
+
+let botaoSortear = 'btn-sortear';
+let botaoReiniciar ='btn-reiniciar';
+    
+let sorteados = [];
+let numero;
+let resultado = document.getElementById('resultado');
 
 function sortear() {
-    let quantidade = parseInt(document.getElementById('quantidade').value);
-    let de = parseInt(document.getElementById('de').value);
-    let ate = parseInt(document.getElementById('ate').value);
+    quantidade = parseInt(document.getElementById('quantidade').value);
+    de = parseInt(document.getElementById('de').value);
+    ate = parseInt(document.getElementById('ate').value);
+
+    let numerosDisponíveis = ate - de + 1;
+
+    if (isNaN(quantidade) || isNaN(de) || isNaN(ate)) {
+        resultado.innerHTML = '<label class="texto__paragrafo">Preencha <strong>todos os campos</strong> acima para realizar o sorteio!</label>';
+
+        desaativarBotão(botaoSortear);
+        ativarBotão(botaoReiniciar);
+
+    } else if (quantidade > numerosDisponíveis) {
+        resultado.innerHTML = '<label class="texto__paragrafo">A quantidade de números sorteados precisa ser <strong>MENOR</strong> que a quantidade de números disponíveis para sorteio</label>';
+
+        desaativarBotão(botaoSortear);
+        ativarBotão(botaoReiniciar);
+     } else if (quantidade == 0) {
+        resultado.innerHTML = '<label class="texto__paragrafo">A quantidade de números sorteados <strong>NÃO PODE</strong> ser igual a ZERO!</label>';
+
+        desaativarBotão(botaoSortear);
+        ativarBotão(botaoReiniciar);
+
+    } else if (de >= ate) {
+        resultado.innerHTML = '<label class="texto__paragrafo">O valor em <strong>"Do número"</strong> precisa ser <strong>MENOR</strong> que o valor em <strong>Até o número</strong></label>';
+
+        desaativarBotão(botaoSortear);
+        ativarBotão(botaoReiniciar);
+
+    } else {
+        for (let i = 1; i <= quantidade; i++) {
+            numero = obterNumeroAleatorio(de,ate);
     
-    let sorteados = [];
-    let numero;
+            while(sorteados.includes(numero)){
+                numero = obterNumeroAleatorio(de,ate);
+            }
+            sorteados.push(numero);
+        }
 
-    for (let i = 0; i <= quantidade; i++) {
-        numero = obterNumeroAleatorio(de,ate);
-        sorteados.push(numero);
+        let textoLista = transformarListaEmString(sorteados);
+        
+        resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${textoLista}</label>`;
+        
+        ativarBotão(botaoReiniciar);
     }
+}
 
-    let resultado = document.getElementById('resultado');
-    resultado.innerHTML = ˋ<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>ˋ;
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    resultado.innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+    
+    ativarBotão(botaoSortear);
+    desaativarBotão(botaoReiniciar);
 }
 
 function obterNumeroAleatorio(min, max) {
     return parseInt(Math.random() * (max - min + 1) + min);
+}
+
+function transformarListaEmString(lista) {
+    let tamanho = lista.length;
+
+    if (tamanho === 0) {
+        return '';
+    } else if (tamanho === 1) {
+        return lista[0].toString();
+    } else if (tamanho === 2) {
+        return lista.join(' e ');
+    } else {
+        return lista.slice(0, -1).join(', ') + ' e ' + lista[lista.length - 1];
+    }
+}
+
+function ativarBotão(id) {
+    let botao = document.getElementById(id);
+    botao.classList.replace('container__botao-desabilitado', 'container__botao');
+}
+
+function desaativarBotão(id) {
+    let botao = document.getElementById(id);
+    botao.classList.replace('container__botao', 'container__botao-desabilitado');
 }
